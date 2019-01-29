bdd436a7fc9c8c927a46d4aba0c37d013022bd05
RajatP314 <34140939+RajatP314@users.noreply.github.com>
2019-01-29 11:14:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let URL = require('url').URL;
 let counter = 0;
 
 const server = http.createServer((req, res) => {
- let u = new URL(req.url);
+ let u = new URL('https://one-counter.herokuapp.com'+req.url);
 if(u.pathname === '/save'){
  counter = parseInt(u.search.substring(1));
  res.send("0");
